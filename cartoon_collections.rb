f8e882997e7f6de34898b5bddcf9917cb5b576b0
Kealoha1988 <kealohaabe1988@gmail.com>
2020-11-13 01:06:59 -0500

    commit tree 6cddc20c478792c8de391f0701fd0f4271b06838

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  dwarves = array
+  blob = dwarves.each_with_index { |name, index| puts "#{index + 1}. #{name}"}
+  blob
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(elements)
+  yay = elements.map{|i| i.capitalize}
+  yo = yay.map{|i| "#{i}!"}
+  yo
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(elements)
+  elements.any?{|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(stuff)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  news = stuff.find{|i| cheese_types.include?(i)}
+  news
+  if news === false
+    puts nil
+  else
+    news
+  end
 end
